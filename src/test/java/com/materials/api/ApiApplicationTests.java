7bc2a573302cefc22df0297dfa0cf1a69c715daa
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-05-17 23:38:45 -0300

    initial commit

---
+++ src/test/java/com/materials/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.materials.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
