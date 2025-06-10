e2727aece12dc21e8c149ae444f02a2d33101aa0
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-06-10 20:07:06 -0300

    refact(TASK-19): GenericEntity created and new attributes implementation in user entity (#17)

--- src/main/java/com/materials/api/controller/dto/UserRequestDTO.java
+++ src/main/java/com/materials/api/controller/dto/UserRequestDTO.java
@@ -7,9 +7,15 @@ import lombok.Getter;
 
 @Getter
 public class UserRequestDTO {
-  @NotEmpty private String name;
-  @NotEmpty @Email private String email;
-  @NotEmpty private String phone;
+  @NotEmpty
+  private String name;
+
+  @NotEmpty
+  @Email
+  private String email;
+
+  @NotEmpty
+  private String registry;
 
   @Pattern(regexp = "^\\d{5}-?\\d{3}$", message = "CEP deve estar no formato XXXXX-XXX ou XXXXXXXX")
   private String postalCode;
--- src/main/java/com/materials/api/service/dto/UserDTO.java
+++ src/main/java/com/materials/api/service/dto/UserDTO.java
@@ -12,6 +12,11 @@ public class UserDTO {
   private Long id;
   private String name;
   private String email;
-  private String phone;
+  private String registry;
+  private Boolean active;
+  private Boolean isDeleted;
+  private String createdAt;
+  private String updatedAt;
+  private String deletedAt;
   private Address address;
 }
