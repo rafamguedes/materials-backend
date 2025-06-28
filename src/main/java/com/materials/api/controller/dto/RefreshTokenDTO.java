096d033d3865a533584a32875be16a216ad78e4b
Rafael Guedes <rafaelmagalhaes1@live.com>
2025-06-28 17:25:08 -0300

    fix(TASK-47): refresh token endpoint created

---
+++ src/main/java/com/materials/api/controller/dto/RefreshTokenDTO.java
@@ -0,0 +1,12 @@
+package com.materials.api.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenDTO {
+  private String refreshToken;
+}
--- src/main/java/com/materials/api/controller/dto/TokenDTO.java
+++ src/main/java/com/materials/api/controller/dto/TokenDTO.java
@@ -4,4 +4,4 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.materials.api.security.Role;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record TokenDTO(String token, Long id, String name, String email, Role role) {}
+public record TokenDTO(String token, String refreshToken, Long id, String name, String email, Role role) {}
